befb06c3f5e0927e602627a8507fc85dbe0d0a61
jimcamut <jimcamut@gmail.com>
2018-07-18 10:23:29 -0700

    check usertype if none exists

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var mandrill = require('mandrill-api/mandrill');
 
+function checkUserType(options) {
+   return new Promise( (resolve, reject) => {
+      if (!options.user.attributes.user_type) {
+         new Parse.Query('User')
+         .equalTo('email', options.user.attributes.email)
+         .first({useMasterKey: true})
+         .then( resp => resolve( resp.get('user_type') ))
+         .catch(err => reject(err));
+      } else {
+         resolve( options.user.attributes.user_type );
+      }
+   });
+}
+
 var MandrillAdapter = mandrillOptions => {
 
   if (
@@ -35,76 +49,81 @@ var MandrillAdapter = mandrillOptions => {
   var mandrill_client = new mandrill.Mandrill(mandrillOptions.apiKey);
 
   var sendVerificationEmail = options => {
-    var global_merge_vars = [
-      { name: 'appname', content: options.appName},
-      { name: 'username', content: options.user.get("username")},
-      { name: 'email', content: options.user.get("email")},
-      { name: 'link', content: options.link}
-    ];
 
-    if (typeof mandrillOptions.customUserAttributesMergeTags !== 'undefined') {
-      for (var extra_attr of mandrillOptions.customUserAttributesMergeTags) {
-        global_merge_vars.push({ name: extra_attr, content: options.user.get(extra_attr) || '' });
+   return new Promise((resolve, reject) => {
+
+      var global_merge_vars = [
+         { name: 'appname', content: options.appName},
+         { name: 'username', content: options.user.get("username")},
+         { name: 'email', content: options.user.get("email")},
+         { name: 'link', content: options.link}
+      ];
+
+      if (typeof mandrillOptions.customUserAttributesMergeTags !== 'undefined') {
+         for (var extra_attr of mandrillOptions.customUserAttributesMergeTags) {
+         global_merge_vars.push({ name: extra_attr, content: options.user.get(extra_attr) || '' });
+         }
       }
-    }
 
-    var message = {
-      from_email: mandrillOptions.fromEmail,
-      from_name: mandrillOptions.displayName,
-      headers: {
-        'Reply-To': mandrillOptions.replyTo
-      },
-      to: [{
-        email: options.user.get("email")
-      }],
-      subject: mandrillOptions.verificationSubject,
-      text: mandrillOptions.verificationBody,
-      global_merge_vars: global_merge_vars
-    }
-
-
-    // Overrides for user types
-    var user_type = options.user.get("user_type");
-    var user_types = mandrillOptions && mandrillOptions.user_types && Object.keys(mandrillOptions.user_types).length ? mandrillOptions.user_types : undefined;
-    if (user_type && user_types) {
-       for (var type in user_types) {
-          if (type === user_type) {
-             var setting = user_types[type];
-             if (setting.from_email) message.from_email = setting.from_email;
-             if (setting.from_name) message.from_name = setting.from_name;
-             if (setting.reply_to) message.headers['Reply-To'] = setting.reply_to;
-             if (setting.app_name) {
-               global_merge_vars[0].content = setting.app_name;
-               message.subject = message.subject.replace(/\*\|appname\|\*/ig, setting.app_name);
-               message.text = message.text.replace(/\*\|appname\|\*/ig, setting.app_name)
-             }
-          }
-       }
-    }
-
-    return new Promise((resolve, reject) => {
-      if (mandrillOptions.verificationTemplateName) {
-        mandrill_client.messages.sendTemplate(
-          {
-            template_name: mandrillOptions.verificationTemplateName,
-            template_content: [],
-            message: message,
-            async: true
-          },
-          resolve,
-          reject
-        )
-      } else {
-        mandrill_client.messages.send(
-          {
-            message: message,
-            async: true
-          },
-          resolve,
-          reject
-        ) 
+      var message = {
+         from_email: mandrillOptions.fromEmail,
+         from_name: mandrillOptions.displayName,
+         headers: {
+         'Reply-To': mandrillOptions.replyTo
+         },
+         to: [{
+         email: options.user.get("email")
+         }],
+         subject: mandrillOptions.verificationSubject,
+         text: mandrillOptions.verificationBody,
+         global_merge_vars: global_merge_vars
       }
-    });
+
+      checkUserType(options)
+         .then( final_user_type => {
+
+            // Overrides for user types
+            var user_type = final_user_type || options.user.get("user_type");
+            var user_types = mandrillOptions && mandrillOptions.user_types && Object.keys(mandrillOptions.user_types).length ? mandrillOptions.user_types : undefined;
+            if (user_type && user_types) {
+               for (var type in user_types) {
+                  if (type === user_type) {
+                     var setting = user_types[type];
+                     if (setting.from_email) message.from_email = setting.from_email;
+                     if (setting.from_name) message.from_name = setting.from_name;
+                     if (setting.reply_to) message.headers['Reply-To'] = setting.reply_to;
+                     if (setting.app_name) {
+                        global_merge_vars[0].content = setting.app_name;
+                        message.subject = message.subject.replace(/\*\|appname\|\*/ig, setting.app_name);
+                        message.text = message.text.replace(/\*\|appname\|\*/ig, setting.app_name)
+                     }
+                  }
+               }
+            }
+
+            if (mandrillOptions.verificationTemplateName) {
+               mandrill_client.messages.sendTemplate(
+               {
+                  template_name: mandrillOptions.verificationTemplateName,
+                  template_content: [],
+                  message: message,
+                  async: true
+               },
+               resolve,
+               reject
+               )
+            } else {
+               mandrill_client.messages.send(
+               {
+                  message: message,
+                  async: true
+               },
+               resolve,
+               reject
+               ) 
+            }
+         });
+      });
   }
 
   var sendPasswordResetEmail = options => {
